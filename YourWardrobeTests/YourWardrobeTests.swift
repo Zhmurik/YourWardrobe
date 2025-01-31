954f2b609c49f2a7b1df4db5309039f68f503d2e
Zhmurik <azhmurkova@gmail.com>
2025-01-31 14:02:47 -0800

    Initial Commit

---
+++ YourWardrobeTests/YourWardrobeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YourWardrobeTests.swift
+//  YourWardrobeTests
+//
+//  Created by Anna Zhmurkova on 1/31/25.
+//
+
+import Testing
+@testable import YourWardrobe
+
+struct YourWardrobeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
