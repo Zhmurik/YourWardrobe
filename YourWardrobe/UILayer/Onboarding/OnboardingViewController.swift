8e450215322c5a4cd6dab104a88e48e6284ed7ff
Zhmurik <azhmurkova@gmail.com>
2025-02-09 00:31:16 -0800

    fix bug pageViewController

--- YourWardrobe/UILayer/Onboarding/OnboardingViewController.swift
+++ YourWardrobe/UILayer/Onboarding/OnboardingViewController.swift
@@ -12,6 +12,8 @@ class OnboardingViewController: UIViewController {
     
     // MARK: - Properties
     private var pages = [OnboardingPartViewController]()
+    private var currentPageIndex: Int = 0
+        
     
     // MARK: - Views
     private let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
@@ -139,11 +141,18 @@ extension OnboardingViewController: UIPageViewControllerDataSource {
 // MARK: - UIPageViewControllerDelegate deledate
 extension OnboardingViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
-        if let index = pages.firstIndex(of: pendingViewControllers.first! as! OnboardingPartViewController) {
-            pageControl.currentPage = index
-            let page = pages[index]
-            let title = page.buttonText
-            bottomButton.setTitle(title, for: .normal)
-            }
+        if let index = pages.firstIndex(of: pendingViewControllers.first! as! OnboardingPartViewController){
+            currentPageIndex = index
         }
     }
+        
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            pageControl.currentPage = currentPageIndex
+            let page = pages[currentPageIndex]
+            let title = page.buttonText
+            bottomButton.setTitle(title, for: .normal)
+        }
+    }
+}
+
