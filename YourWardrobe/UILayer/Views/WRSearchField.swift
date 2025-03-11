93a1db775860ef54a7b6f3cd9f2c759d6bfbb520
Anna Zhmurkova <azhmurkova@gmail.com>
2025-03-10 23:22:21 -0700

    create searchField on the main screen. add picture "search", integrate in the main

--- YourWardrobe/UILayer/Views/WRSearchField.swift
+++ YourWardrobe/UILayer/Views/WRSearchField.swift
@@ -24,8 +24,12 @@ class WRSearchField: UITextField {
         self.layer.borderColor = AppColors.accentColor.cgColor
         self.font = UIFont.Oswald.Regular.size(size: 14)
         self.placeholder = "Search"
+        let imageView = UIImageView(frame: CGRect(x: 20, y: 0, width: 20, height: 20))
+        imageView.image = UIImage(resource: .search)
+        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 20 + 20 + 10, height: 20))
+        leftPaddingView.addSubview(imageView)
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: self.frame.height))
-        self.leftView = paddingView
+        self.leftView = leftPaddingView
         self.rightView = paddingView
         self.leftViewMode = .always
         self.rightViewMode = .always
